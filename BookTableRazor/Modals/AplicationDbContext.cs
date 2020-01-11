764a0ed20c025b3447b5e2e9836e7b1ec01c3bff
jahedul29 <jahedulh1@gmail.com>
2020-01-11 20:56:19 +0600

    Creating a New Page

---
+++ BookTableRazor/Modals/AplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookTableRazor.Modals
+{
+    public class AplicationDbContext:DbContext
+    {
+        public AplicationDbContext(DbContextOptions<AplicationDbContext> options):base(options)
+        {
+            
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
---
+++ BookTableRazor/Modals/Book.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookTableRazor.Modals
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+        [Display(Name = "Book Name")]
+        [Required]
+        [StringLength(20)]
+        public string Name { get; set; }
+        [StringLength(15)]
+        [Required]
+        public string ISBN { get; set; }
+        [Required]
+        [Display(Name = "Author of Book")]
+        public string Author { get; set; }
+    }
+}
