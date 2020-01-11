8997f0d24d67734fbe91300024f473dfed6539f3
jahedul29 <jahedulh1@gmail.com>
2020-01-12 00:54:38 +0600

    Completed The Full Table

--- BookTableRazor/Pages/BookList/Index.cshtml.cs
+++ BookTableRazor/Pages/BookList/Index.cshtml.cs
@@ -2,16 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BookTableRazor.Modals;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace BookTableRazor.Pages.BookList
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
-        {
+        private readonly AplicationDbContext _db;
 
+        public IndexModel(AplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IEnumerable<Book> Books { get; set; }
+        public async Task OnGet()
+        {
+            Books = await _db.Books.ToListAsync();
         }
     }
 }
