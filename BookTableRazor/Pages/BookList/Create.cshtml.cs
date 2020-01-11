635fd76b2785088729d715d96105a9ff1e7b1b58
jahedul29 <jahedulh1@gmail.com>
2020-01-11 22:08:07 +0600

    Create Page part-1

---
+++ BookTableRazor/Pages/BookList/Create.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookTableRazor.Modals;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BookTableRazor.Pages.BookList
+{
+    public class CreateModel : PageModel
+    {
+        [BindProperty]
+        public Book Book { get; set; }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
