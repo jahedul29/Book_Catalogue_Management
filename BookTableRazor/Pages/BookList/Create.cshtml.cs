d659dca5c44341ad108887f5119f2e9f3ec98048
jahedul29 <jahedulh1@gmail.com>
2020-01-11 23:15:41 +0600

    AddingFunctionalityOfCreateButtonAndValidationForNameTextbox

--- BookTableRazor/Pages/BookList/Create.cshtml.cs
+++ BookTableRazor/Pages/BookList/Create.cshtml.cs
@@ -10,12 +10,31 @@ namespace BookTableRazor.Pages.BookList
 {
     public class CreateModel : PageModel
     {
+        private readonly AplicationDbContext _db;
+
+        public CreateModel(AplicationDbContext db)
+        {
+            _db = db;
+        }
+
         [BindProperty]
         public Book Book { get; set; }
 
         public void OnGet()
         {
 
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _db.Books.Add(Book);
+            await _db.SaveChangesAsync();
+            return RedirectToPage("Index");
         }
     }
 }
